43dd443d77843729807938baa04de9c424f4f56c
thatsmyjam <thatsmyjam438@gmail.com>
2016-05-21 13:15:01 -0400

    Main now handles alloc, free, view, qq. Debug ignored in new gitignore.

--- main.c
+++ main.c
@@ -9,12 +9,50 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define COMMAND_ALLOC "alloc";
+
+void handle_alloc();
+void handle_free();
+void handle_view();
+
 int main(int argc, char** argv)
 {
-	char command[10];
+	char command[256];
 
-	while (1) {
+	while (1)
+	{
 		printf("Enter command: ");
 		scanf("%s", command);
+		if (strcmp(command, "alloc") == 0)
+		{
+			printf("Handling alloc...\r\n");
+			handle_alloc();
+		}
+		if (strcmp(command, "free") == 0) {
+			printf("Handling free...\r\n");
+			handle_free();
+		}
+		if (strcmp(command, "view") == 0) {
+			printf("Handling view...\r\n");
+			handle_view();
+		}
+		if (strcmp(command, "q") == 0) {
+			break;
+		}
 	}
+}
+
+void handle_free()
+{
+
+}
+
+void handle_alloc()
+{
+
+}
+
+void handle_view()
+{
+
 }
