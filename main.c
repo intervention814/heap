1314c4729dc8a90bac09bcb75a218005103f80b5
thatsmyjam <thatsmyjam438@gmail.com>
2016-05-21 13:07:11 -0400

    First commit

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: May 21, 2016
+ *      Author: Freeman
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+	char command[10];
+
+	while (1) {
+		printf("Enter command: ");
+		scanf("%s", command);
+	}
+}
