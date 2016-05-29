d32fe3ba7c39ebe23a1853d454910c5fc98c5fc9
Your Name <you@example.com>
2016-05-28 20:31:35 -0400

    Simple free coalesce with 'prev' block contigously partially implemented, not handling updating of next ptr/double coalescence yet

--- api_heap.c
+++ api_heap.c
@@ -12,12 +12,6 @@
 
 char* g_heap_ptr;
 
-typedef struct HeapHeader {
-	size_t data_size;
-	struct HeapHeader* next;
-	struct HeapHeader* prev;
-} HeapHeader;
-
 int api_init(size_t underlying_heap_size) {
 	printf("Initializing heap with %lu bytes\r\n", underlying_heap_size);
 	g_heap_ptr = malloc(underlying_heap_size + sizeof(HeapHeader));
@@ -28,7 +22,7 @@ int api_init(size_t underlying_heap_size) {
 	HeapHeader* hdr = (HeapHeader*)g_heap_ptr;
 	hdr->data_size = underlying_heap_size;
 	hdr->next = NULL;
-	hdr->prev = NULL;
+	//hdr->prev = NULL;
 	return 1;
 }
 
@@ -39,12 +33,12 @@ void api_view() {
 	}
 	HeapHeader* cur = (HeapHeader*)g_heap_ptr;
 	while (cur != NULL) {
-		printf("Addr: [%p] : Size: [%lu] (header size [%lu]) : Next: [%p] : Prev: [%p]\r\n",
+		printf("Addr: [%p] : Size: [%lu] (header size [%lu]) : Next: [%p]\r\n",
 				cur,
 				cur->data_size,
 				sizeof(HeapHeader),
-				cur->next,
-				cur->prev);
+				cur->next);
+				//cur->prev);
 		cur = cur->next;
 	}
 }
@@ -64,10 +58,10 @@ void* api_alloc(size_t size) {
 			HeapHeader* newHeaderRet = (HeapHeader*)((char*)(cur + cur->data_size + sizeof(HeapHeader)));
 			newHeaderRet->data_size = size;
 			newHeaderRet->next = NULL;
-			newHeaderRet->prev = NULL;
+			//newHeaderRet->prev = NULL;
 			//g_heap_ptr = (char*)newHeaderRet;
 
-			printf("Created new 'only block left' at %p (size %lu)\r\n", g_heap_ptr, newHeaderRet->data_size);
+			printf("Created new 'from only block left' hdr at %p (size %lu)\r\n", (char*)newHeaderRet, newHeaderRet->data_size);
 			return (char*)newHeaderRet + sizeof(HeapHeader);
 		}
 	}
@@ -77,7 +71,56 @@ void* api_alloc(size_t size) {
 }
 
 void api_free(void* ptr) {
+	HeapHeader* hdr = (HeapHeader*)(ptr - sizeof(HeapHeader));
+	printf("Freeing block of size: [%lu] at [%p]\r\n", hdr->data_size, (unsigned char*)hdr);
+
+	HeapHeader* prevContigousBlock = api_get_prev_coalesc_block(hdr);
+	if (prevContigousBlock == NULL) {
+		printf("Cannot coalesce free'd ptr with prev\r\n");
+	}
+	else {
+		printf("Coalescing prev block at [%p] (size [%lu], hdr [%lu]) "
+				"with free'd block hdr at [%p]\r\n",
+				prevContigousBlock,
+				prevContigousBlock->data_size,
+				sizeof(HeapHeader),
+				hdr);
+		api_coallesce_prev(prevContigousBlock, hdr);
+	}
 	return;
+}
+
+void api_coallesce_prev(HeapHeader* prev, HeapHeader* hdr) {
+	if (prev->next == NULL) {
+		printf("Prev block's next is NULL\r\n");
+		prev->data_size += sizeof(HeapHeader) + hdr->data_size;
+		return;
+	}
+}
+
+HeapHeader* api_get_prev_coalesc_block(HeapHeader* hdr) {
+	if (g_heap_ptr == NULL) {
+		printf("No blocks in free list.\r\n");
+		return NULL;
+	}
+	HeapHeader* cur = (HeapHeader*)g_heap_ptr;
+	while (cur != NULL) {
+		unsigned char* nextAddress = (unsigned char*)(cur + cur->data_size + sizeof(HeapHeader));
+		if (nextAddress == (unsigned char*)hdr) {
+			printf("Found heap block in free list contiguously "
+					"before [%p] beginning at [%p], size [%lu]\r\n",
+					hdr,
+					cur,
+					cur->data_size);
+			return cur;
+		}
+		cur = cur->next;
+		if (cur == (HeapHeader*)g_heap_ptr) {
+			printf("We've come back to the start of the free list without finding a block to coalesce with.\r\n");
+			break;
+		}
+	}
+	return NULL;
 }
 
 size_t api_get_size(const unsigned char* ptr) {
--- api_heap.h
+++ api_heap.h
@@ -8,9 +8,17 @@
 #ifndef API_HEAP_H_
 #define API_HEAP_H_
 
+typedef struct HeapHeader {
+	size_t data_size;
+	struct HeapHeader* next;
+} HeapHeader;
+
 int api_init(size_t underlying_heap_size);
 void api_view();
 void* api_alloc(size_t size);
 void api_free(void* ptr);
 size_t api_get_size(const unsigned char* ptr);
+HeapHeader* api_get_prev_coalesc_block(HeapHeader* hdr);
+void api_coallesce_prev(HeapHeader* prev, HeapHeader* hdr);
+
 #endif /* API_HEAP_H_ */
